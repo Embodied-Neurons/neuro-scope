d50f2027bb954da38517fc496e433f7baf185f11
glartek2 <60228656+glartek2@users.noreply.github.com>
2025-03-22 00:15:39 +0100

    Fix: Correct Graph Representation
    now get_structure returnes graph constructed based on model and input

--- ExtractionTest.py
+++
@@ -1,9 +0,0 @@
-from SimpleMnist import model
-import ExtractData as ed
-
-
-print(ed.get_model_structure(model))
-#print("/\/\/\/\/\/\/\/\/\/\/\/\/\/")
-#print(ed.get_activations(model))
-#print("==========================")
-#print(ed.get_gradients(model))
